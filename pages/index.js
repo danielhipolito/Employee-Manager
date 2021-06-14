1a259fb978dadb785eb585ae8a37f6929c47be8d
Danielhipolito <daniel@healonsite.mx>
2021-06-13 21:06:12 -0500

    Added Currency Select Feature

--- pages/index.js
+++ pages/index.js
@@ -8,10 +8,15 @@ import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 const Index = () => {
     const [showAddEmployee, setShowAddEmployee] = useState(false);
+    const [currencyConfig, setCurrencyConfig] = useState({currency:"MXN",languague:"mx-MX"});
     const [employees, setEmployees] = useState([]);
 
     let headValues = ["Empleado","Nombre","Empresa","Área","Salario Mensual","Imagen","Acciones"];
     let bodyValues = [];
+    let options1 = { style: 'currency', currency: currencyConfig.currency };
+    let numberFormat1 = new Intl.NumberFormat(currencyConfig.languague, options1);
+    // const options1 = { style: 'currency', currency: 'USD' };
+    // const numberFormat1 = new Intl.NumberFormat('us-US', options1);
 
     if(!employees || employees.length == 0) {
         bodyValues.push(<tr>
@@ -19,7 +24,11 @@ const Index = () => {
         </tr>);
     }
     else {
-        employees.forEach(employee => {   
+        employees.forEach(employee => {  
+            let salary = numberFormat1.format(employee.salary); 
+            if(currencyConfig.currency == "USD" ) 
+            salary =`${(parseFloat(employee.salary)/21.50).toFixed(2)} USD`;
+
             bodyValues.push(<tr key = {"s"}>
                 <td className = "text-primary"> {employee.id ?
                     employee.id.substring(0,5)
@@ -27,7 +36,7 @@ const Index = () => {
                 <td> {employee.name} </td>
                 <td> {employee.company} </td>
                 <td> {employee.area} </td>
-                <td> {employee.salary} </td>
+                <td> {salary}</td>
                 <td> </td>
                 <td> <a href = "#">
                     <FontAwesomeIcon icon={faEdit} className={"fa-lg text-primary"} /> 
@@ -50,6 +59,13 @@ const Index = () => {
         getEmployees();
         setShowAddEmployee(false);
     };
+
+    const toUsdCurrency = () => {
+        setCurrencyConfig({currency:'USD', languague:'en-US'});
+    };
+    const toMxnCurrency = () => {
+        setCurrencyConfig({currency:'MXN', languague:'mx-MX'});
+    };
    
     return <Layout>
         <BasicModal show ={showAddEmployee} onClose ={()=>setShowAddEmployee(false)} 
@@ -61,6 +77,30 @@ const Index = () => {
         <div className ="row d-flex justify-content-center">
             <div className = "col-lg-10">
                 <div className = "row">
+                    <div className ="col-lg-12">
+                        <div className = "row d-flex align-items-center">
+                            <div className ="col-lg-4"/>
+                            <div className ="col-lg-4 text-center">
+                                <b className ="text-secondary">Divisa de salario</b>
+                                <p className="text-danger"> {`${currencyConfig.currency}`} </p>
+                            </div>
+                            <div className ="col-lg-4 text-right mb-3">
+                                <b className ="mr-5 text-secondary">Seleccionar</b>
+                                <div className ="mt-1 mr-4">
+                                    <button onClick = {toUsdCurrency} 
+                                        className ={`mr-2 btn ${currencyConfig.currency == "USD" 
+                                        ?'btn-primary':''}`}>
+                                        USD
+                                    </button>
+                                    <button onClick = {toMxnCurrency} 
+                                        className ={`mr-2 btn ${currencyConfig.currency == "MXN" 
+                                        ?'btn-primary':''}`}>
+                                        MXN
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                     <div className="col-lg-6"/>
                     <div className = "col-lg-6 text-right">
                         <button className ="btn btn-primary mr-3" onClick = {()=>setShowAddEmployee(true)}>
